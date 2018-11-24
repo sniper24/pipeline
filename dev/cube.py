1012d46e421af6f4097710d6c142716137b248a3
brian <brianxie89@gmail.com>
2018-11-24 11:55:40 +0800

    Update cube.py

--- dev/cube.py
+++ dev/cube.py
@@ -1,7 +1,35 @@
 import win32com.client
 win32c = win32com.client.constants
+'''
+1. load_data
+	just load data from csv to df, get a schema list
+2. build_cube
+	aggragate by different levels and columns then 
+'''
 
-def get_pivot(wb, PivotTableName, PivotSourceRange, filters, cols, rols, fields):# PivotTargetRange, 
+
+
+'''
+def load_data():
+	# return df
+	pass
+'''
+
+def build_cube(from_df, to_csv, group_by_cols, max_cols, sum_cols):
+	#from_df
+
+	# group_by_cols
+	# max(max_cols)
+	# sum(sum_cols)
+
+	pass
+
+def connect_csv(wb, from_csv, connection_name):
+	# wb.add_new_connection(connection_name, from_csv)
+	pass
+
+
+def pivot(wb, PivotTableName, PivotSourceRange, filters, cols, rols, fields):# PivotTargetRange, 
 	# Add a new worksheet
 	wb.Sheets.Add (After=wb.Sheets(wb.Worksheets.Count))
 	sheet_new = wb.Worksheets(wb.Worksheets.Count)
@@ -13,7 +41,7 @@ def get_pivot(wb, PivotTableName, PivotSourceRange, filters, cols, rols, fields)
 	PivotCache = wb.PivotCaches().Create(SourceType=win32c.xlDatabase, SourceData=PivotSourceRange, Version=win32c.xlPivotTableVersion14)
 	PivotTable = PivotCache.CreatePivotTable(TableDestination=PivotTargetRange, TableName=PivotTableName, DefaultVersion=win32c.xlPivotTableVersion14)
 
-	print "get_pivot|PivotName:{}|ws.count:{}|filters:{}|cols:{}|rols:{}|fields:{}".format(PivotTableName, wb.Worksheets.Count, "|".join(filters),"|".join(cols),"|".join(rols),"|".join(fields))
+	print "Generating PivotTable: [{}]|ws.count:[{}]|filters:[{}]|cols:[{}]|rols:[{}]|fields:[{}]".format(PivotTableName, wb.Worksheets.Count, "|".join(filters),"|".join(cols),"|".join(rols),"|".join(fields))
 
 	# wb.Sheets.Add (After=wb.Sheets(wb.Worksheets.Count))
 	# sheet_new = wb.Worksheets(2)
@@ -28,6 +56,7 @@ def get_pivot(wb, PivotTableName, PivotSourceRange, filters, cols, rols, fields)
 	for i in range(len(cols)):
 		PivotTable.PivotFields(cols[i]).Orientation = win32c.xlColumnField
 		PivotTable.PivotFields(cols[i]).Position = i+1
+		PivotTable.PivotFields(cols[i]).Subtotals = [False, False, False, False, False, False, False, False, False, False, False, False]
 	for i in range(len(fields)):
 		DataField = PivotTable.AddDataField(PivotTable.PivotFields(fields[i]))
 		# DataField.NumberFormat = '#\'##0.00'
@@ -35,15 +64,6 @@ def get_pivot(wb, PivotTableName, PivotSourceRange, filters, cols, rols, fields)
 		DataField.Function = win32c.xlSum#xlCount #win32c.xlAverage # win32c.xlCount #win32c.xlSum
 		# DataField.Function
 		# https://docs.microsoft.com/en-us/office/vba/api/excel.xlconsolidationfunction
-
-def build_cube(from_df, to_csv, group_by_cols, max_cols, sum_cols):
-	#from_df
-
-	# group_by_cols
-	# max(max_cols)
-	# sum(sum_cols)
-
-	pass
 
 
 Excel = win32com.client.gencache.EnsureDispatch('Excel.Application')
@@ -77,9 +97,11 @@ PivotSourceRange = Sheet1.Range(cl1,cl2)
 
 PivotSourceRange.Select()
 
-get_pivot(wb, 'PivotTable1', PivotSourceRange, filters=["Country", "Gender"], cols=["Sign"], rols=["Name"], fields=["Amount"])
-get_pivot(wb, 'PivotTable2', PivotSourceRange, filters=["Sign", "Gender"], cols=["Country"], rols=["Name"], fields=["Amount"])
-get_pivot(wb, 'PivotTable3', PivotSourceRange, filters=["Country"], cols=["Sign", "Gender"], rols=["Name"], fields=["Amount"])
+pivot(wb, 'PivotTable1', PivotSourceRange, filters=["Country", "Gender"], cols=["Sign"], rols=["Name"], fields=["Amount"])
+pivot(wb, 'PivotTable2', PivotSourceRange, filters=["Sign", "Gender"], cols=["Country"], rols=["Name"], fields=["Amount"])
+pivot(wb, 'PivotTable3', PivotSourceRange, filters=["Country"], cols=["Sign", "Gender"], rols=["Name"], fields=["Amount"])
+pivot(wb, 'PivotTable4', PivotSourceRange, filters=["Sign"], cols=["Country", "Gender"], rols=["Name"], fields=["Amount"])
+pivot(wb, 'PivotTable5', PivotSourceRange, filters=["Gender"], cols=["Country"], rols=["Name"], fields=["Amount", "Amount"])
 
 wb.SaveAs(r'C:\Users\brian\Desktop\ranges_and_offsets2.xlsx')
 
