5325a526a7668019ca3d2f56e7a194c4b2fdae5e
brian <brianxie89@gmail.com>
2018-11-24 15:02:17 +0800

    Update cube.py

--- dev/cube.py
+++ dev/cube.py
@@ -1,5 +1,6 @@
 import win32com.client
 win32c = win32com.client.constants
+import pandas as pd
 '''
 1. load_data
 	just load data from csv to df, get a schema list
@@ -7,13 +8,23 @@ win32c = win32com.client.constants
 	aggragate by different levels and columns then 
 '''
 
+def connect_csv(wb, from_path, to_sheet, start_cell=[1,1]):
+	Sheet1 = wb.Worksheets(to_sheet)
 
+	# load csv to a nested list
+	df = pd.read_csv(from_path)
+	TestData =  [df.columns.tolist()] + df.values.tolist()
+	
+	for i, TestDataRow in enumerate(TestData):
+		for j, TestDataItem in enumerate(TestDataRow):
+			Sheet1.Cells(i+start_cell[0],j+start_cell[1]).Value = TestDataItem
 
-'''
-def load_data():
-	# return df
-	pass
-'''
+	cl1 = Sheet1.Cells(start_cell[0],start_cell[1])
+	cl2 = Sheet1.Cells(start_cell[0]+len(TestData)-1,start_cell[1]+len(TestData[0])-1)
+	PivotSourceRange = Sheet1.Range(cl1,cl2)
+
+	PivotSourceRange.Select()
+	return PivotSourceRange
 
 def build_cube(from_df, to_csv, group_by_cols, max_cols, sum_cols):
 	#from_df
@@ -22,10 +33,6 @@ def build_cube(from_df, to_csv, group_by_cols, max_cols, sum_cols):
 	# max(max_cols)
 	# sum(sum_cols)
 
-	pass
-
-def connect_csv(wb, from_csv, connection_name):
-	# wb.add_new_connection(connection_name, from_csv)
 	pass
 
 
@@ -53,14 +60,17 @@ def pivot(wb, PivotTableName, PivotSourceRange, filters, cols, rols, fields):# P
 	for i in range(len(rols)):
 		PivotTable.PivotFields(rols[i]).Orientation = win32c.xlRowField
 		PivotTable.PivotFields(rols[i]).Position = i+1
+		# PivotTable.PivotFields(rols[i]).Name = 'ColX'
 	for i in range(len(cols)):
 		PivotTable.PivotFields(cols[i]).Orientation = win32c.xlColumnField
 		PivotTable.PivotFields(cols[i]).Position = i+1
 		PivotTable.PivotFields(cols[i]).Subtotals = [False, False, False, False, False, False, False, False, False, False, False, False]
 	for i in range(len(fields)):
 		DataField = PivotTable.AddDataField(PivotTable.PivotFields(fields[i]))
+		# , "Sum of Uptime", xlSum
 		# DataField.NumberFormat = '#\'##0.00'
 		DataField.NumberFormat = '###0.00'
+		DataField.Name = 'Total Amt'
 		DataField.Function = win32c.xlSum#xlCount #win32c.xlAverage # win32c.xlCount #win32c.xlSum
 		# DataField.Function
 		# https://docs.microsoft.com/en-us/office/vba/api/excel.xlconsolidationfunction
@@ -73,36 +83,15 @@ wb = Excel.Workbooks.Add()
 print "wb.sheets.count:{}".format(wb.Worksheets.Count)
 Sheet1 = wb.Worksheets("Sheet1")
 
-TestData = [['Country','Name','Gender','Sign','Amount'],
-			 ['CH','Max' ,'M','Plus',123.4567],
-			 ['CH','Max' ,'M','Minus',-23.4567],
-			 ['CH','Max' ,'M','Plus',12.2314],
-			 ['CH','Max' ,'M','Minus',-2.2314],
-			 ['CH','Sam' ,'M','Plus',453.7685],
-			 ['CH','Sam' ,'M','Minus',-53.7685],
-			 ['CH','Sara','F','Plus',777.666],
-			 ['CH','Sara','F','Minus',-77.666],
-			 ['DE','Hans','M','Plus',345.088],
-			 ['DE','Hans','M','Minus',-45.088],
-			 ['DE','Paul','M','Plus',222.455],
-			 ['DE','Paul','M','Minus',-22.455]]
-
-for i, TestDataRow in enumerate(TestData):
-	for j, TestDataItem in enumerate(TestDataRow):
-		Sheet1.Cells(i+2,j+4).Value = TestDataItem
-
-cl1 = Sheet1.Cells(2,4)
-cl2 = Sheet1.Cells(2+len(TestData)-1,4+len(TestData[0])-1)
-PivotSourceRange = Sheet1.Range(cl1,cl2)
-
-PivotSourceRange.Select()
-
+PivotSourceRange = connect_csv(wb, r"D:/GitRepo/pipeline/dev/test.csv", "Sheet1", start_cell = [4,2])
 pivot(wb, 'PivotTable1', PivotSourceRange, filters=["Country", "Gender"], cols=["Sign"], rols=["Name"], fields=["Amount"])
 pivot(wb, 'PivotTable2', PivotSourceRange, filters=["Sign", "Gender"], cols=["Country"], rols=["Name"], fields=["Amount"])
 pivot(wb, 'PivotTable3', PivotSourceRange, filters=["Country"], cols=["Sign", "Gender"], rols=["Name"], fields=["Amount"])
 pivot(wb, 'PivotTable4', PivotSourceRange, filters=["Sign"], cols=["Country", "Gender"], rols=["Name"], fields=["Amount"])
 pivot(wb, 'PivotTable5', PivotSourceRange, filters=["Gender"], cols=["Country"], rols=["Name"], fields=["Amount", "Amount"])
+Excel.DisplayAlerts = False
+wb.Worksheets("Sheet1").Delete()
+wb.SaveAs(r'D:\GitRepo\pipeline\dev\output.xlsx')
 
-wb.SaveAs(r'C:\Users\brian\Desktop\ranges_and_offsets2.xlsx')
-
+Excel.DisplayAlerts = True
 Excel.Application.Quit()
