3ab7dce30b2032b5097420548da0272d8a36b95b
brian <brianxie89@gmail.com>
2018-11-24 15:54:00 +0800

    Update cube2.py

--- dev/cube2.py
+++ dev/cube2.py
@@ -1,14 +1,30 @@
 import pandas as pd
 import numpy as np
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-def build_cube(from_df, to_path, group_by_cols, max_cols, sum_cols):
-	df = pd.read_csv("./nba.csv")
-	print df
-	# group_by_cols
-	# max(max_cols)
-	# sum(sum_cols)
+def build_cube(from_df, to_path, group_by_cols, max_cols, sum_cols, avg_cols):
+	df = from_df
+	
+	agg_dict = {}
+	for col in max_cols:
+		if col not in agg_dict:
+			agg_dict[col] = {}
+		agg_dict[col]["MAX"] = np.max
+	for col in sum_cols:
+		if col not in agg_dict:
+			agg_dict[col] = {}
+		agg_dict[col]["SUM"] = np.sum
+	for col in avg_cols:
+		if col not in agg_dict:
+			agg_dict[col] = {}
+		agg_dict[col]["AVG"] = np.mean
 
-	pass
+	dfg = df.groupby(group_by_cols).agg(agg_dict)
+	dfg = dfg.reset_index()
+
+	dfg.columns = ['_'.join(col).strip("_") for col in dfg.columns.values]
+	dfg.to_csv(to_path, index=False)
 """
 Age                    int64
 Conference            object
@@ -24,7 +40,8 @@ Team                  object
 Weight                object
 Real_value           float64
 """
-if __name__ == '__main__':
+
+def test():
 	df = pd.read_csv("./nba.csv")
 
 	dfg = df.groupby(['Conference', 'Team']).agg({'Age':
@@ -35,4 +52,9 @@ if __name__ == '__main__':
 	dfg.columns = [' '.join(col).strip() for col in dfg.columns.values]
 	dfg.to_csv('week_grouped.csv', index=False)
 
-	#print df.dtypes
+def main():
+	df = pd.read_csv("./nba.csv")
+	build_cube(df, 'week_grouped.csv', ['Conference', 'Team'], ['Age'], ['Real_value'],[])
+
+if __name__ == '__main__':
+	main()
